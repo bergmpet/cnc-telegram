1aa05bc4b6390361b89dcb701167b37b5c417de8
Petr Bergmann <bergmpet@fel.cvut.cz>
2020-01-06 22:39:59 +0100

    Implementation of getting new messages for bot

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import requests
-import json 
+import json
 from authorization import TOKEN
 
 URL = "https://api.telegram.org/bot{}".format(TOKEN)
@@ -13,6 +13,24 @@ def get_status():
         print("Connection does not work")
 
 
-if __name__ == "__main__":
-    get_status()
+def get_json(url):
+    request = requests.get(url)
+    if request.status_code == 200:
+        return json.loads(request.content)
+    return None
 
+
+def get_updates():
+    answer = get_json(URL + "/getUpdates")
+    if answer is None and answer["ok"]:
+        return
+
+    # testing
+    print(answer)
+    data = answer["result"][0]
+    print(data)
+    print(data["message"])
+
+
+if __name__ == "__main__":
+    get_updates()
