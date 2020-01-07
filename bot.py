39af9589f246591fa83b01862bfc7b95ec309976
Petr Bergmann <bergmpet@fel.cvut.cz>
2020-01-07 01:43:42 +0100

    Correction of a typo in help message

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ DIR_HTML = "<b>Directory Listing</b>\n<i>Dir: {}</i>\n<b>Items:</b>\n\n"
 USR_HTML = "<b>Active Users Listing</b>\n<b>Users:</b>\n\n"
 PCS_HTML = "<b>Running Processes Listing</b>\n<b>Processes:</b>\n\n"
 WRT_HTML = "<b>Write Status: </b>"
-HELP = "Telegram CnC BOT\nUse:\nDirectory listing: ls [path]\nActive users: users\nRunning processes: processes\nWrite to file: write [path] [data]\n\n Exit application: terminate\n"
+HELP = "Telegram CnC BOT\nUse:\nDirectory listing: ls [path]\nActive users: users\nRunning processes: processes\nWrite to file: write [path] [data]\n\nExit application: terminate\n"
 MAX_MESSAGE_LENGTH = 4000
 
 
